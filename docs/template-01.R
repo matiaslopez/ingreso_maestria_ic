b89bfe997056420d0a3f95171c7c086818e13095
Matías López <matiaslopez@gmail.com>
2021-02-01 09:30:44 -0300

    Agregamos el template

---
+++ docs/template-01.R
@@ -0,0 +1,75 @@
+
+# 1. Se desea tener un programa que dada la variable `grados`, que representa 
+# la temperatura en grados Farenheit, calcule en otra variable el valor en Celsius. 
+# Más info https://www.lmgtfy.es/?q=formula+formula+fahrenheit+a+celsius.
+
+# COMPLETAR ACÁ
+
+
+# 2. Escribir otro programa que se comporte a la inversa, es decir, que dada una variable 
+# que represente la temperatura en Celsius, calcule su equivalente en Farenheit.
+
+# COMPLETAR ACÁ...
+
+
+# 3. Escribir un conversor de kilometros a millas.
+
+# ....
+
+
+# 4. Dado un cuadrado, que el largo de su base se encuentra guardado en una variable 
+# llamada `base`, calcular:
+#   a. El perímetro
+#   b. El área
+
+
+
+# 5. Idem anterior pero para un triangulo equilatero.
+
+
+
+# 6. Asumiendo que los años tienen siempre 365 días, calcular:
+#  a. cuántos días vas a cumplir tu próximo cumpleaños,
+#  b. cuántas horas vas a haber vivido,
+#  c. y cuántos segundos.
+
+
+
+# 7. Se tienen las notas de 3 materias en sus respectivas variables: `matematica`, 
+# `lengua`, `dibujo`. Calcular el promedio de dichas notas.
+
+
+
+# 8. Repetir el ítem anterior, pero ahora con los valores guardados en un vector llamado
+#  `notas`.  Hint: se puede acceder a los elementos de un vector con `[]`. 
+#  Ej: `c(4, 6, 88)[2]` nos da el valor `6`.
+
+
+
+# 9. Si en el ítem anterior no usaste la función `lenght()` y `sum()`, 
+# volvé a resolverlo usandolas.
+
+
+
+# 10. Si en el ítem anterio al anterior no usaste la función `mean()`, 
+#  volvé a resolverlo usandola.
+
+
+
+# 11. Dadas dos variables, `perro` y `gato` escribir un programa que intercambie los 
+#  valores de ambas variables.
+
+
+
+# 12. Calcular el índice de masa corporal (IMC) de una persona cuya altura es `1.78`m y 
+#  su peso es `80`kg. (`IMC = peso / altura^2 `)
+
+
+
+# 13. Si tenemos los pesos y las alturas de personas en 2 vectores, calcular el IMC para cada uno.
+
+
+
+# 14. Sobre el cálculo del ejercicio anterior, encontrar el valor máximo, el mínimo, 
+#  el promedio y la mediana de los IMCs.
+
